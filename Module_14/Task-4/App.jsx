04903f01a8d32ec5e49535f05c805f0aaf4c8118
Dev Doshi <doshidev58@gmail.com>
2025-04-22 17:53:29 +0530

    Update App.jsx

--- Module_14/Task-4/App.jsx
+++ Module_14/Task-4/App.jsx
@@ -1,1 +1,13 @@
+import React from 'react';
+import Counter from './Counter';
 
+const App = () => {
+  return (
+    <div>
+      <h1>My React App</h1>
+      <Counter />
+    </div>
+  );
+};
+
+export default App
