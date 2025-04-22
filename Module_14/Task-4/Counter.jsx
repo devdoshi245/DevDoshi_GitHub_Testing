b6e44d593d2cd8b9285668e67ec574b37047363e
Dev Doshi <doshidev58@gmail.com>
2025-04-22 17:52:39 +0530

    Update Counter.jsx

--- Module_14/Task-4/Counter.jsx
+++ Module_14/Task-4/Counter.jsx
@@ -1,1 +1,20 @@
+import React, { useState } from 'react';
 
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const increaseCount = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div>
+      <h1>Counter Example</h1>
+      <hr />
+      <h3>Current Count: {count}</h3>
+      <button onClick={increaseCount}>Increment</button>
+    </div>
+  );
+};
+
+export default Counter
