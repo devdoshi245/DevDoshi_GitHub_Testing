e607c67139df977bf22571d659b33912cee98155
Dev Doshi <doshidev58@gmail.com>
2025-04-22 17:49:10 +0530

    Update App.jsx

--- Module_14/Task-3/App.jsx
+++ Module_14/Task-3/App.jsx
@@ -1,1 +1,14 @@
+import React from 'react';
+import UserCard from './UserCard';
 
+const App = () => {
+  return (
+    <div>
+      <UserCard name="John Doe" age="28" location="New York" />
+      <UserCard name="Jane Smith" age="32" location="California" />
+      <UserCard name="Alice Johnson" age="25" location="Texas" />
+    </div>
+  );
+}
+
+export default App;
