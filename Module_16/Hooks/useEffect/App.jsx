d16792880ebe9af4b2544b71931f10f67c598605
Dev Doshi <doshidev58@gmail.com>
2025-05-09 19:33:05 +0530

    Update App.jsx

--- Module_16/Hooks/useEffect/App.jsx
+++ Module_16/Hooks/useEffect/App.jsx
@@ -1,1 +1,13 @@
+import React from 'react';
+import Example from './Example';
 
+function App() {
+  return (
+    <div>
+      <h2>useEffect Hook Example</h2>
+      <Example />
+    </div>
+  );
+}
+
+export default App;
