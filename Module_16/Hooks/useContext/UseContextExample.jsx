fd2bbc8c7e937a6452ce5d48895f1b435713676a
Dev Doshi <doshidev58@gmail.com>
2025-05-09 19:39:10 +0530

    Update UseContextExample.jsx

--- Module_16/Hooks/useContext/UseContextExample.jsx
+++ Module_16/Hooks/useContext/UseContextExample.jsx
@@ -1,1 +1,46 @@
+import React, { createContext, useCallback, useContext } from 'react'
+import { useState } from 'react';
+let userData = createContext();
 
+const UseContextExample = () => {
+    const [name, setName] = useState("Testing");
+  return (
+    <div>
+        <h3>First Component called...</h3>
+      <h3>Name is --- {name}</h3>
+      <userData.Provider value={name}>
+      <Component2/>
+      </userData.Provider>
+    </div>
+  )
+}
+
+const Component2 = () =>{
+    return(
+        <div>
+            <h3>Second Component called...</h3>
+            <Component3/>
+        </div>
+    )
+}
+
+const Component3 = () =>{
+    return(
+        <div>
+            <h3>Third Component called...</h3>
+            <Component4/>
+        </div>
+    )
+}
+
+const Component4 = () =>{
+    let name = useContext(userData);
+    return(
+        <div>
+            <h3>Fourth & Final Component called...</h3>
+            <h3>Final name is --- {name}</h3>
+        </div>
+    )
+}
+
+export default UseContextExample
