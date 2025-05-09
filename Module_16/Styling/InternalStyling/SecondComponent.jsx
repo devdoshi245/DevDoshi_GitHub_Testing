dfb9cb4a20ac799a63eebf8d9173e0d7d330a291
Dev Doshi <doshidev58@gmail.com>
2025-05-09 20:04:06 +0530

    Update SecondComponent.jsx

--- Module_16/Styling/InternalStyling/SecondComponent.jsx
+++ Module_16/Styling/InternalStyling/SecondComponent.jsx
@@ -1,1 +1,15 @@
+import React from 'react'
 
+const SecondComponent = () => {
+    const internal = {
+        color:"pink",
+        backgroundColor:"cadetblue"
+    }
+  return (
+    <div>
+      <h3 style={internal}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus commodi veritatis nulla voluptatum saepe quibusdam doloribus neque beatae reprehenderit earum sunt repellat quasi nesciunt facilis illo, repudiandae alias explicabo sed!</h3>
+    </div>
+  )
+}
+
+export default SecondComponent
