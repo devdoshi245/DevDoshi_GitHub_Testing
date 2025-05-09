f462ac14faf07c7b28904f88e6921537878761b7
Dev Doshi <doshidev58@gmail.com>
2025-05-09 20:06:50 +0530

    Update ThirdComponent.jsx

--- Module_16/Styling/ExternalStyling/ThirdComponent.jsx
+++ Module_16/Styling/ExternalStyling/ThirdComponent.jsx
@@ -1,1 +1,12 @@
+import React from 'react'
+import './External.css'
 
+const ThirdComponent = () => {
+  return (
+    <div>
+      <h3 className='abc'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde sint nisi facere distinctio saepe nam, nemo illo, labore maiores debitis iure itaque deleniti sit libero, accusantium excepturi autem sunt placeat.</h3>
+    </div>
+  )
+}
+
+export default ThirdComponent
