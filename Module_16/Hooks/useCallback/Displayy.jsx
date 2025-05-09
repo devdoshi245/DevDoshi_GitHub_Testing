d83f6a96b5b5e6ec893ef996c462da228d9828a3
Dev Doshi <doshidev58@gmail.com>
2025-05-09 19:46:44 +0530

    Update Displayy.jsx

--- Module_16/Hooks/useCallback/Displayy.jsx
+++ Module_16/Hooks/useCallback/Displayy.jsx
@@ -1,1 +1,41 @@
+import React from 'react'
+import { memo } from 'react';
+
+const Displayy = ({data}) => {
+    console.log("Display component called...");
+    
+  return (
+    <div>
+      <table border={2}>
+        <thead>
+          <tr>
+            <td>Id</td>
+            <td>Name</td>
+            <td>Age</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            data.map((i,index)=>{
+              return(
+                <tr>
+                  <td>{index+1}</td>
+                  <td>{i.name}</td>
+                  <td>{i.age}</td>
+                  <td>
+                    <button onClick={()=>editData(index)}>Edit</button>
+                    <button onClick={()=>delData(index)}>Delete</button>
+                  </td>
+                </tr>
+              )
+            })
+          }
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default memo(Displayy)
 
