14b0b32c51a196cf9d2533d7b6c07e092c31a726
Dev Doshi <doshidev58@gmail.com>
2025-05-09 19:32:34 +0530

    Update Example.jsx

--- Module_16/Hooks/useEffect/Example.jsx
+++ Module_16/Hooks/useEffect/Example.jsx
@@ -1,1 +1,15 @@
+import React, { useEffect } from 'react';
 
+function Example() {
+  useEffect(() => {
+    console.log('Component mounted');
+  }, []);
+
+  return (
+    <div>
+      <h1>Hello from useEffect</h1>
+    </div>
+  );
+}
+
+export default Example;
