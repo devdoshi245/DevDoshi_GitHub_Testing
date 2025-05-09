0f8e85170ce6ea8a94fc2e1774a59f67bb216104
Dev Doshi <doshidev58@gmail.com>
2025-05-09 19:48:14 +0530

    Update App.jsx

--- Module_16/Hooks/useCallback/App.jsx
+++ Module_16/Hooks/useCallback/App.jsx
@@ -1,9 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import UseCallback from './usecallback'
-import Memo_Example from './Memo_Example'
 
 function App() {
   return (
