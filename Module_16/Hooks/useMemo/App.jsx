5376b5e477f2ad79f28209fa178093fe79e3f793
Dev Doshi <doshidev58@gmail.com>
2025-05-09 19:50:38 +0530

    Update App.jsx

--- Module_16/Hooks/useMemo/App.jsx
+++ Module_16/Hooks/useMemo/App.jsx
@@ -1,1 +1,14 @@
+import './App.css'
+import Memo_Example from './Memo_Example'
 
+function App() {
+  return (
+    <>
+      <div>
+        <Memo_Example />
+      </div>
+    </>
+  )
+}
+
+export default App
