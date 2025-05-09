23e78c904064e1a4e6fa703477f07b0034be64e9
Dev Doshi <doshidev58@gmail.com>
2025-05-09 20:08:58 +0530

    Update App.js

--- Module_16/Styling/StyledComponent/App.js
+++ Module_16/Styling/StyledComponent/App.js
@@ -1,1 +1,17 @@
+import './App.css';
+import styled from 'styled-components'
 
+function App() {
+  const H3 = styled.h3`
+      color: white;
+      background-color: black;
+      text-align: center;
+  `
+  return (
+    <div>
+      <H3>Lorem ipsum dolor sit amet consectetur adipisicing eltionem natus temporibus ab placeat. Distinctio, modi libero!</H3>
+    </div>
+  );
+}
+
+export default App;
