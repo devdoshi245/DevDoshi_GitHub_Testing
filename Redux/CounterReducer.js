4d535e1d5b869ec17f93a786f922398f71b8fb3c
Dev Doshi <doshidev58@gmail.com>
2025-05-09 23:21:06 +0530

    Update CounterReducer.js

--- Redux/CounterReducer.js
+++ Redux/CounterReducer.js
@@ -1,1 +1,19 @@
+import { dec, inc } from "./CounterAction"
 
+const initialState = {
+    count:0,
+    data:[]
+}
+const CounterReducer = (state=initialState,action)=>{
+    switch(action.type){
+        case inc:return {
+            count: state.count+1
+        }
+        case dec:return {
+            count: state.count-1
+        }
+        default: return state
+    }
+}
+
+export default CounterReducer
