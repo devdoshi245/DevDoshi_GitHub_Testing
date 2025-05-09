0d7d950bfca10b5fdffc62ee6fc9b05587daaa6b
Dev Doshi <doshidev58@gmail.com>
2025-05-09 19:58:11 +0530

    Update App.jsx

--- Module_16/Routing/App.jsx
+++ Module_16/Routing/App.jsx
@@ -1,1 +1,27 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Menu from './Menu'
+import {BrowserRouter, Route, Routes} from 'react-router'
+import Home from './Home'
+import Contact from './Contact'
+import Timepass from './Timepass'
 
+function App() {
+  return (
+    <>
+      <div>
+        <Menu />
+        <BrowserRouter>
+          <Routes>
+            <Route path='/home' element={<Home />}/>
+            <Route path='/contact' element={<Contact />}/>
+            <Route path='/timepass' element={<Timepass />}/>
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </>
+  )
+}
+
+export default App
