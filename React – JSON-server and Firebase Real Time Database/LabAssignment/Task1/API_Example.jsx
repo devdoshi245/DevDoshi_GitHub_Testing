996a02df31003561a5c99e1f38b120564b235cbd
Dev Doshi <doshidev58@gmail.com>
2025-05-09 23:01:46 +0530

    Update API_Example.jsx

--- React – JSON-server and Firebase Real Time Database/LabAssignment/Task1/API_Example.jsx
+++ React – JSON-server and Firebase Real Time Database/LabAssignment/Task1/API_Example.jsx
@@ -1,1 +1,42 @@
+import React, { useEffect, useState } from 'react'
 
+const API_Example = () => {
+    const [data, setData] = useState([]);
+    useEffect(()=>{
+        fetch('https://fakestoreapi.com/products')
+        .then(response => response.json())
+        .then(ab => setData(ab));
+    },[])
+  return (
+    <div>
+        <table border={2}>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Image</th>
+                    <th>product</th>
+                    <th>Category</th>
+                    <th>Price</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    data.map((i)=>{
+                        return(
+                            <tr>
+                                <td>{i.id}</td>
+                                <td><img src={i.image} height={'50px'} width={'50px'}/></td>
+                                <td>{i.title}</td>
+                                <td>{i.category}</td>
+                                <td>{i.price}</td>
+                            </tr>
+                        )
+                    })
+                }
+            </tbody>
+        </table>
+    </div>
+  )
+}
+
+export default API_Example
