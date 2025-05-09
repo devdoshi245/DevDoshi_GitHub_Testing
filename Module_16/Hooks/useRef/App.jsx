addc28e9b8a6a5d528cd4966773b10a923b8259e
Dev Doshi <doshidev58@gmail.com>
2025-05-09 19:36:37 +0530

    Update App.jsx

--- Module_16/Hooks/useRef/App.jsx
+++ Module_16/Hooks/useRef/App.jsx
@@ -1,1 +1,15 @@
+import './App.css'
+import UseRefExample from './UseRefExample'
 
+function App() {
+
+  return (
+    <>
+      <div>
+        <UseRefExample />
+      </div>
+    </>
+  )
+}
+
+export default App
