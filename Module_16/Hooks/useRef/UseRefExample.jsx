3b763504417a5af1d8114038d23a15035d6d971e
Dev Doshi <doshidev58@gmail.com>
2025-05-09 19:36:07 +0530

    Update UseRefExample.jsx

--- Module_16/Hooks/useRef/UseRefExample.jsx
+++ Module_16/Hooks/useRef/UseRefExample.jsx
@@ -1,1 +1,22 @@
+import React from 'react'
+import { useState } from 'react';
+import { useRef } from 'react';
 
+const UseRefExample = () => {
+    const [count, setCount] = useState(0);
+    let a = useRef(10);
+    const addCounter = ()=>{
+        setCount(count+1);
+        a.current = a.current+1;
+        console.log(a);
+    }
+  return (
+    <div>
+      <h3>Count is -- {count}</h3>
+      <button onClick={addCounter}>Add Counter</button>
+      <h3>A is --- {a.current}</h3>
+    </div>
+  )
+}
+
+export default UseRefExample
