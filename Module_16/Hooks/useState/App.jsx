7984edefc8eb7a7766ffbb047b29c81683fc922f
Dev Doshi <doshidev58@gmail.com>
2025-05-09 19:29:03 +0530

    Update App.jsx

--- Module_16/Hooks/useState/App.jsx
+++ Module_16/Hooks/useState/App.jsx
@@ -1,1 +1,13 @@
+import React from 'react';
+import Counter from './Counter';
 
+function App() {
+  return (
+    <div>
+      <h2>Simple Counter App</h2>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
