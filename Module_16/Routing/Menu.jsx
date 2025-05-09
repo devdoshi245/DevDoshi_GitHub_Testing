6b2777e11d0e3b728c64e7c4263ddf5f5623373e
Dev Doshi <doshidev58@gmail.com>
2025-05-09 19:57:43 +0530

    Update Menu.jsx

--- Module_16/Routing/Menu.jsx
+++ Module_16/Routing/Menu.jsx
@@ -1,1 +1,17 @@
+import React from 'react'
 
+const Menu = () => {
+  return (
+    <div>
+      <ul type='none'>
+        <li>
+            <a href="/home">Home</a>||
+            <a href="/contact">Contact</a>||
+            <a href="/timepass">Timepass</a>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default Menu
