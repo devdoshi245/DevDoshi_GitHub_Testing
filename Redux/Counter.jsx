26815814946e9581b8beec592ef2840f6f5d7498
Dev Doshi <doshidev58@gmail.com>
2025-05-09 23:20:45 +0530

    Update Counter.jsx

--- Redux/Counter.jsx
+++ Redux/Counter.jsx
@@ -1,1 +1,20 @@
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { add, minus } from './redux/CounterAction'
 
+const Counter = () => {
+    const count = useSelector((i)=>i.count)
+    const dispatch = useDispatch()
+    const minusCounter = ()=>{
+        dispatch(minus())
+    }
+  return (
+    <div>
+      <h3>Count is -- {count}</h3>
+      <button onClick={()=>dispatch(add())}>Add</button>
+      <button onClick={minusCounter}>Minus</button>
+    </div>
+  )
+}
+
+export default Counter
