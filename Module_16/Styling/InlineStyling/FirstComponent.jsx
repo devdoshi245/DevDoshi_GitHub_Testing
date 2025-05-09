ca464eab499cd403665c32f143f26d4e5671b9fc
Dev Doshi <doshidev58@gmail.com>
2025-05-09 20:01:19 +0530

    Update FirstComponent.jsx

--- Module_16/Styling/InlineStyling/FirstComponent.jsx
+++ Module_16/Styling/InlineStyling/FirstComponent.jsx
@@ -1,1 +1,11 @@
+import React from 'react'
 
+const FirstComponent = () => {
+  return (
+    <div>
+      <h3 style={{color:"red", backgroundColor:"yellow"}}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis voluptatem, quae delectus nemo non eos nobis facere ipsam dolores, possimus et. Velit aperiam eveniet possimus? Enim, facere. Sit, necessitatibus aut.</h3>
+    </div>
+  )
+}
+
+export default FirstComponent
