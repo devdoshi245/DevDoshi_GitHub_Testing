bab6301cda335f1526938e195de5be751c96e29d
Dev Doshi <doshidev58@gmail.com>
2025-05-09 19:53:32 +0530

    Update App.jsx

--- Module_16/Hooks/CustomHooks/App.jsx
+++ Module_16/Hooks/CustomHooks/App.jsx
@@ -1,1 +1,14 @@
+import './App.css'
+import CustomHooks from './CustomHooks'
 
+function App() {
+  return (
+    <>
+    <div>
+      <CustomHooks />
+    </div>
+    </>
+  )
+}
+
+export default App
