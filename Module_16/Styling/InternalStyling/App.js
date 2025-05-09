f2a35128d1d5d84f21e4484b44e8d688085e07a1
Dev Doshi <doshidev58@gmail.com>
2025-05-09 20:04:55 +0530

    Update App.js

--- Module_16/Styling/InternalStyling/App.js
+++ Module_16/Styling/InternalStyling/App.js
@@ -1,1 +1,12 @@
+import './App.css';
+import SecondComponent from './SecondComponent';
 
+function App() {
+  return (
+    <div>
+      <SecondComponent />
+    </div>
+  );
+}
+
+export default App;
