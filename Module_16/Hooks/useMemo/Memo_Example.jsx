b8abfeb3677641aff84df4c4ae52324c50446c38
Dev Doshi <doshidev58@gmail.com>
2025-05-09 19:49:53 +0530

    Update Memo_Example.jsx

--- Module_16/Hooks/useMemo/Memo_Example.jsx
+++ Module_16/Hooks/useMemo/Memo_Example.jsx
@@ -1,1 +1,42 @@
+import React, { useMemo } from 'react'
+import { useState } from 'react';
 
+const Memo_Example = () => {
+    const [count, setCount] = useState(0);
+    const [num, setNum] = useState(0);
+
+    const addCounter = ()=>{
+        console.log("counter func called...");
+        
+        setCount(count+1);
+    }
+
+    const addNum = () =>{
+        console.log("number func called...");
+        
+        setNum(num+10);
+    }
+
+    const smthng = (count) =>{
+        console.log("smthng called...");
+        for(let i=0;i<10000;i++)
+        {
+            count+=i;
+        }
+        return count;
+    }
+    const calc = useMemo(()=>{
+        return smthng(count);
+    },[count]);
+  return (
+    <div>
+        <h3>Count is : {count}</h3>
+        <button onClick={addCounter}>Add</button>
+        
+        <h3>Num is : {num}</h3>
+        <button onClick={addNum}>Add</button>
+    </div>
+  )
+}
+
+export default Memo_Example
