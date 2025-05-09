cde199b32c1e68554d3c8ec070df8bb42036b249
Dev Doshi <doshidev58@gmail.com>
2025-05-09 23:20:03 +0530

    Update CounterAction.js

--- Redux/CounterAction.js
+++ Redux/CounterAction.js
@@ -1,1 +1,12 @@
-
+export const inc = 'INCREMENT'
+export const dec = 'DECREMENT'
+export const add = ()=>{
+    return {
+        type:inc
+    }
+}
+export const minus = ()=>{
+    return {
+        type:dec
+    }
+}
